57bed44439acde9e33412edf44ba02bee3de2d51
Jules NEUVILLE <jules.neuville.etu@univ-lille.fr>
2026-05-19 16:28:51 +0200

    Migrate from Conda to us.

--- launch_gesture_speech.sh
+++ launch_gesture_speech.sh
@@ -1,4 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
-source .venv/bin/activate
-python launcher.py
+
+install_uv() {
+    echo "uv non trouvé"
+    echo "Tentative d'installation ..."
+    wget https://astral.sh/uv/install.sh && chmod +x ./install.sh && ./install.sh && rm install.sh && echo "uv installé !"
+}
+
+setup_venv() {
+    uv venv --python 3.10
+}
+
+sync() {
+    echo "Synchronisation des dépendances"
+    uv sync
+    echo "Activation du venv"
+    source .venv/bin/activate
+}
+
+launch() {
+    echo "Lancement en cours..."
+    uv run --with jupyter launcher.py
+}
+
+nil=/dev/null
+
+uv help 1>$nil 2>$nil || install_uv
+test -d .venv || setup_venv
+sync && launch
